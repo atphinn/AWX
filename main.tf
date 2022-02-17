926dc4e3578c69f62916ed722053b06ff23faead
AP <r3tr0@pop-os.localdomain>
2022-02-17 16:37:07 -0500

    updated security rules for both port 22 and port 80

--- main.tf
+++ main.tf
@@ -68,7 +68,7 @@ resource "azurerm_network_security_group" "awxterraformnsg" {
 
   security_rule {
     name                       = "SSH"
-    priority                   = 1001
+    priority                   = 101
     direction                  = "Inbound"
     access                     = "Allow"
     protocol                   = "Tcp"
@@ -77,6 +77,20 @@ resource "azurerm_network_security_group" "awxterraformnsg" {
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
+
+  security_rule {
+    name                       = "HTTP"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+
 
   tags = {
     environment = "Asibel Tower"
